657ae89f7ecfdee69dcce8fcd5cc2f05325a2166
Jomelgar <jomelgar1903@gmail.com>
2025-10-15 22:59:38 -0600

    EmailJS -> PowerAutomate

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,51 +1,43 @@
 import emailjs from 'emailjs-com';
 
-export function sendVerificationCode(email, code) {
-  const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
-  const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID_VERIFICATION;
-  const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
-
-  const params = {
+export async function sendVerificationCode(email, code) {
+  await fetch(import.meta.env.VITE_URL_CODE, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
     email: email,
-    verification_code: code,
-  };
+    verification_code: code
+  })
+});
 
-  return emailjs.send(serviceId, templateId, params, publicKey);
 }
 
-export function sendNotificationStudent(request_type,id,section,url,first_name,last_name, email, message) {
-  const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
-  const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID_NOTIFICATION;
-  const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
-
-  const full_name = first_name + " " + last_name;
-  const params = {
-    name: full_name,
+export async function sendNotificationStudent(request_type,id,section,url,first_name,last_name, email, message) {
+  return await fetch(import.meta.env.VITE_URL_NOTIFICATION, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    name: first_name + " " + last_name,
     message: message,
     email: email,
     section: section,
-    id: id,
     type: request_type,
-    url_case: url
-  };
-
-  return emailjs.send(serviceId, templateId, params, publicKey);
+    url_case: url,
+    })
+  });
 }
 
-export function sendNotificationUser(request_type,id,section,url,email, message) {
-  const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
-  const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID_NOTIFICATION;
-  const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
-
-  const params = {
+export async function sendNotificationUser(request_type,id,section,url,email, message) {
+  return await fetch(import.meta.env.VITE_URL_NOTIFICATION, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
     name: email,
     message: message,
     email: email,
     section: section,
-    id: id,
     type: request_type,
     url_case: url,
-  };
-
-  return emailjs.send(serviceId, templateId, params, publicKey);
+    })
+  });
 }
